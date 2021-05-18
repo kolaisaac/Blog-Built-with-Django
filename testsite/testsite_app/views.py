0d0edb3950e15513c96b38f7ec3e04e626839820
kolaisaac <blestisaac10@gmail.com>
2021-05-18 15:13:44 +0100

    Django Tag Sample :rocket:

--- testsite/testsite_app/views.py
+++ testsite/testsite_app/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_view(request):
+    car = "Toyota"
     return render(
         request,
-        "home.html"
+        "home.html",
+        {"car": car}
     )
