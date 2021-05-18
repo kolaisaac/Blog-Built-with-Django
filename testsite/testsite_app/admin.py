3f417e342dc9208730bf6faf536f6fce37e2dafe
kolaisaac <blestisaac10@gmail.com>
2021-05-18 17:35:26 +0100

    Creating models :rocket:

--- testsite/testsite_app/admin.py
+++ testsite/testsite_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import CarShop
 # Register your models here.
+
+
+class CarShopDB(admin.ModelAdmin):
+    list_display = [
+        "car_name","car_manufacturer","year_produce"
+    ]
+
+admin.site.register(CarShop, CarShopDB)
--- testsite/testsite_app/models.py
+++ testsite/testsite_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class CarShop(models.Model):
+    class Meta:
+        verbose_name = "Car Shop"
+        verbose_name_plural = "Car Shop"
+    car_name = models.CharField(max_length=20)
+    car_manufacturer = models.CharField(max_length=12)
+    year_produce = models.CharField(max_length=4)
